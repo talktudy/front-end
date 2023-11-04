cf266fb37f1e9048b10da599d84c8176aac67bb4
prgmr99 <johnyeom24@naver.com>
2023-11-04 14:00:55 +0900

    [fix] 에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	base: '',
+	define: {
+		global: {},
+	},
 	build: {
 		minify: false,
 	},
